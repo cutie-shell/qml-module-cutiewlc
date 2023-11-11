37a3b2c56c5daef9d628f7fea6741203b7fe83f3
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2023-11-11 20:36:44 +0200

    Import qt6-foreign-toplevel-management as well

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,5 +1,6 @@
 #include "plugin.h"
 #include <wayland-cutie-shell.h>
+#include <qt6-foreign-toplevel-management.h>
 
 void CutieWlcPlugin::registerTypes(const char *uri) {
     CutieShell *cutieShell = new CutieShell();
@@ -9,4 +10,15 @@ void CutieWlcPlugin::registerTypes(const char *uri) {
         Q_UNUSED(scriptEngine)
         return cutieShell;
     });
+
+    ForeignToplevelManagerV1 *foreignToplevelManager = new ForeignToplevelManagerV1();
+    qmlRegisterSingletonType<ForeignToplevelManagerV1>(uri, 1, 0, "ForeignToplevelManagerV1", 
+        [foreignToplevelManager](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
+        Q_UNUSED(engine)
+        Q_UNUSED(scriptEngine)
+        return foreignToplevelManager;
+    });
+
+    qmlRegisterUncreatableType<ForeignToplevelHandleV1>(uri, 1, 0, "ForeignToplevelHandleV1",
+        "This type is used to interact with a foreign toplevel. Use ForeignToplevelManagerV1 to retrieve and detect these toplevels.");
 }
