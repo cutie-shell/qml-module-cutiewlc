641f9deb2004d1bad26f53f1df8f63f4defc7672
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2023-11-11 22:32:42 +0200

    Use regular types instead of singletons.
    Using regular types will allow the program to only connect protocols it needs.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -3,22 +3,8 @@
 #include <qt6-foreign-toplevel-management.h>
 
 void CutieWlcPlugin::registerTypes(const char *uri) {
-    CutieShell *cutieShell = new CutieShell();
-    qmlRegisterSingletonType<CutieShell>(uri, 1, 0, "CutieWlc", 
-        [cutieShell](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-        Q_UNUSED(engine)
-        Q_UNUSED(scriptEngine)
-        return cutieShell;
-    });
-
-    ForeignToplevelManagerV1 *foreignToplevelManager = new ForeignToplevelManagerV1();
-    qmlRegisterSingletonType<ForeignToplevelManagerV1>(uri, 1, 0, "ForeignToplevelManagerV1", 
-        [foreignToplevelManager](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-        Q_UNUSED(engine)
-        Q_UNUSED(scriptEngine)
-        return foreignToplevelManager;
-    });
-
+    qmlRegisterType<CutieShell>(uri, 1, 0, "CutieWlc");
+    qmlRegisterType<ForeignToplevelManagerV1>(uri, 1, 0, "ForeignToplevelManagerV1");
     qmlRegisterUncreatableType<ForeignToplevelHandleV1>(uri, 1, 0, "ForeignToplevelHandleV1",
         "This type is used to interact with a foreign toplevel. Use ForeignToplevelManagerV1 to retrieve and detect these toplevels.");
 }
