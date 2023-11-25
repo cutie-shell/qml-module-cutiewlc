dbb4d9b9befcab66270c650b6398df162c6706f7
Erik Inkinen <erik.inkinen@erikinkinen.fi>
2023-11-25 17:44:02 +0200

    plugin: Import qt6-output-power-management as well

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,10 +1,12 @@
 #include "plugin.h"
 #include <wayland-cutie-shell.h>
 #include <qt6-foreign-toplevel-management.h>
+#include <qt6-output-power-management.h>
 
 void CutieWlcPlugin::registerTypes(const char *uri) {
     qmlRegisterType<CutieShell>(uri, 1, 0, "CutieWlc");
     qmlRegisterType<ForeignToplevelManagerV1>(uri, 1, 0, "ForeignToplevelManagerV1");
     qmlRegisterUncreatableType<ForeignToplevelHandleV1>(uri, 1, 0, "ForeignToplevelHandleV1",
         "This type is used to interact with a foreign toplevel. Use ForeignToplevelManagerV1 to retrieve and detect these toplevels.");
+    qmlRegisterType<OutputPowerManagerV1>(uri, 1, 0, "OutputPowerManagerV1");
 }
